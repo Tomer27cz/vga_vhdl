139602f4400f90569381da5a97d46443eebd13bf
Tomer27cz <85194189+Tomer27cz@users.noreply.github.com>
2026-03-31 17:17:42 +0200

    add debounce to rst button

--- top/pattern_only/src/pattern_only_top.vhd
+++ top/pattern_only/src/pattern_only_top.vhd
@@ -51,18 +51,39 @@ architecture behavioral of pattern_only_top is
         );
     end component img_gen;
 
-    signal ce       : std_logic;
-    signal h_count  : std_logic_vector(9 downto 0);
-    signal v_count  : std_logic_vector(9 downto 0);
-    signal video_on : std_logic;
+    component debounce is
+        port (
+            clk         : in  std_logic;
+            rst         : in  std_logic;
+            btn_in      : in  std_logic;
+            btn_state   : out std_logic;
+            btn_press   : out std_logic
+        );
+    end component debounce;
+
+    signal ce             : std_logic;
+    signal h_count        : std_logic_vector(9 downto 0);
+    signal v_count        : std_logic_vector(9 downto 0);
+    signal video_on       : std_logic;
+    signal btnd_debounced : std_logic;
 begin
+
+    -- Debouncer for the BTND input
+    debounce_0 : debounce
+    port map (
+        clk         => CLK100MHZ,
+        rst         => '0',
+        btn_in      => BTND,
+        btn_state   => btnd_debounced,
+        btn_press   => open
+    );
 
     -- Generate 25 MHz pixel clock enable pulse
     clk_en_0 : clk_en
     generic map (G_MAX => 4)  -- 100 MHz / 4 = 25 MHz
     port map (
         clk   => CLK100MHZ,
-        rst   => BTND,
+        rst   => btnd_debounced,
         ce    => ce
     );
 
@@ -70,7 +91,7 @@ begin
     vga_sync_0 : vga_sync
     port map (
         clk      => CLK100MHZ,
-        rst      => BTND,
+        rst      => btnd_debounced,
         ce       => ce,
         hsync    => VGA_HS,
         vsync    => VGA_VS,
@@ -83,7 +104,7 @@ begin
     img_gen_0 : img_gen
     port map (
         clk      => CLK100MHZ,
-        rst      => BTND,
+        rst      => btnd_debounced,
         ce       => ce,
         h_count  => h_count,
         v_count  => v_count,
